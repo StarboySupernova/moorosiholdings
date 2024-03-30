c7154f8a8495e6d1ddccfb14b6f7f29be5489bda
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-03-30 17:41:35 +0200

    BlogItemStyles reverted back to the code from Duke

--- web/src/styles/blog/BlogItemStyles.js
+++ web/src/styles/blog/BlogItemStyles.js
@@ -3,13 +3,6 @@ import styled from 'styled-components';
 export const BlogItemStyles = styled.div`
   display: inline-block;
   border-radius: 12px;
-  padding: 10px;
-  background: rgba(255, 255, 255, 0.3);
-  border: 0.5px solid rgba(255, 255, 255, 0.5);
-  box-shadow: 0px 50px 100px rgba(0, 0, 0, 0.25);
-  backdrop-filter: blur(20px);
-  /* Note: backdrop-filter has minimal browser support */
-  border-radius: 60px 0px 60px 60px;
   .img {
     height: 250px;
     margin-bottom: 1.5rem;
@@ -23,7 +16,6 @@ export const BlogItemStyles = styled.div`
   }
   .publishedAt {
     margin-bottom: 0.3rem;
-    color: var(--darkBlue);
   }
   &:hover {
     .img [data-main-image] {
@@ -32,24 +24,10 @@ export const BlogItemStyles = styled.div`
   }
   .categoriesText {
     a {
-      color: var(--white);
+      color: var(--gray);
       &:hover {
         text-decoration: underline;
-        background: linear-gradient(91.4deg, #2fb8ff 0%, #9eecd9 100%);
-        box-shadow: 0px 20px 40px rgba(147, 231, 221, 0.3);
-        border-radius: 30px;
       }
-      padding: 4px;
-      background: linear-gradient(
-        180deg,
-        rgba(24, 32, 79, 0.4) 0%,
-        rgba(24, 32, 79, 0.25) 100%
-      );
-      border: 0.5px solid var(--white);
-      box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.15);
-      backdrop-filter: blur(20px);
-      /* Note: backdrop-filter has minimal browser support */
-      border-radius: 10px;
     }
   }
   @media only screen and (max-width: 768px) {
@@ -57,23 +35,4 @@ export const BlogItemStyles = styled.div`
       margin-bottom: 0.5rem;
     }
   }
-`;
-
-export const ContainerDiv = styled.div`
-  position: relative;
-  height: 100%;
-  overflow: visible;
-`;
-
-export const BackgroundBlogDiv = styled.div`
-  background: linear-gradient(180deg, #75a698 11.94%, #23333f 80.98%);
-  backdrop-filter: blur(20px);
-  /* Note: backdrop-filter has minimal browser support */
-  border-radius: 60px 0px 60px 60px;
-  transform: matrix(0.99, 0.14, -0.11, 0.99, 0, 0);
-  position: absolute;
-  width: 360px;
-  height: 423px;
-  right: 20px;
-  z-index: -10;
 `;
